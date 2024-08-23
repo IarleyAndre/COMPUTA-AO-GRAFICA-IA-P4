5de3b50263f2f692c784e44fa80295c986e12d76
Iarley André <128102571+IarleyAndre@users.noreply.github.com>
2024-08-23 19:40:01 -0300

    Add files via upload

---
+++ computaçaoGraficaRealidadeV/script.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+    //Seleciona o canvas pelo Id 
+    var canvas = document.getElementById('myCanvas');
+    var ctx = canvas.getContext('2d');
+    var squareSize = 60;
+    var x = 100;
+    var y = 100;
+
+    ctx.fillStyle = 'green';
+    ctx.fillRect(x, y , squareSize,squareSize);
+    
+};
